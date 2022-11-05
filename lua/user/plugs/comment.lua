0065253e0660e0153715ce2512de10956418a32b
Hector Realpe <hectorluis.realpe@scalian.com>
2022-11-05 09:21:02 +0100

    all well setted and working propperly

--- lua/user/plugs/comment.lua
+++ lua/user/plugs/comment.lua
@@ -3,39 +3,31 @@ if not status_ok then
   return
 end
 
-local status_ok_1, _ = pcall(require, "lsp-inlayhints")
-if not status_ok_1 then
-  return
-end
-
 comment.setup {
-  ignore = "^$",
   pre_hook = function(ctx)
-    -- For inlay hints
-    local line_start = (ctx.srow or ctx.range.srow) - 1
-    local line_end = ctx.erow or ctx.range.erow
-    require("lsp-inlayhints.core").clear(0, line_start, line_end)
+    local U = require "Comment.utils"
 
-    require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-
-    if vim.bo.filetype == "javascript" or vim.bo.filetype == "typescript" then
-      local U = require "Comment.utils"
-
-      -- Determine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-      if ctx.ctype == U.ctype.blockwise then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
-
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = type,
-        location = location,
-      }
+    local status_utils_ok, utils = pcall(require, "ts_context_commentstring.utils")
+    if not status_utils_ok then
+      return
     end
+
+    local location = nil
+    if ctx.ctype == U.ctype.block then
+      location = utils.get_cursor_location()
+    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+      location = utils.get_visual_start_location()
+    end
+
+    local status_internals_ok, internals = pcall(require, "ts_context_commentstring.internals")
+    if not status_internals_ok then
+      return
+    end
+
+    return internals.calculate_commentstring {
+      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+      location = location,
+    }
   end,
 }
+
